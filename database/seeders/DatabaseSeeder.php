24d7359ab80991563f95e7d4874695ddc3f66e1a
fachrianalbar <fachry.albar29@gmail.com>
2026-06-09 15:16:40 +0700

    refactor: remove CustomerSeeder and FleetSeeder from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             MenuSeeder::class,
-            CustomerSeeder::class,
-            FleetSeeder::class,
         ]);
     }
 }
